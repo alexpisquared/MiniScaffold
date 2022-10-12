ea0b200d69d4075112d545d1f73f015c0733aa1d
AlexPi <alex.pigida@outlook.com>
2022-10-12 15:48:37 -0400

    Update MvvmInitHelper.cs

--- Src/MinNavTpl/MinNavTpl.VM/MvvmInitHelper.cs
+++ Src/MinNavTpl/MinNavTpl.VM/MvvmInitHelper.cs
@@ -34,10 +34,23 @@ public static class MvvmInitHelper
     _ = services.AddTransient<Page03VM>();
     _ = services.AddTransient<Page02VM>();
 
- //!!!   _ = services.AddTransient<ISecForcer, CI.SecurityEnforcement.Mok.SecForcer>();
+    _ = services.AddTransient<ISecForcer, /*CI.SecurityEnforcement.Mok.*/SecForcer>();
     _ = services.AddTransient<SqlPermissionsManager>();
     _ = services.AddTransient<BmsPermissionsManager>();
 
     _ = services.AddTransient<UserSettings>();
   }
+}
+
+public class SecForcer : ISecForcer
+{
+  public SecForcer() : this(true, true) { }
+  public SecForcer(bool isRead, bool isEdit) => (CanRead, CanEdit) = (isRead, isEdit);
+
+  public bool CanRead { get; }
+  public bool CanEdit { get; }
+
+  public string PermisssionCSV => $"{(CanRead ? "Read+" : "")}{(CanEdit ? "Edit+" : "")}".Trim(new[] { '+', ' ' });
+
+  public bool HasAccessTo(PermissionFlag ownedPermissions, PermissionFlag resource) => ownedPermissions.HasFlag(resource);
 }
