36912afcc6f816928544f547d3ea1fbbbfcf8ac5
AlexPi <alex.pigida@outlook.com>
2022-10-29 12:07:04 -0400

    main + spec

---
+++ Src/MinNavTpl/MinNavTpl.View.Spec/Page03View.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MinNavTpl.View.Spec;
+/// <summary>
+/// Interaction logic for Page03View.xaml
+/// </summary>
+public partial class Page03View : UserControl
+{
+  public Page03View()
+  {
+    InitializeComponent();
+  }
+}
