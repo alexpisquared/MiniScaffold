36700ae158d556d4555a1febe7bf8b114f0c3e7c
AlexPi <alex.pigida@outlook.com>
2022-11-05 17:04:34 -0400

    pau

--- Src/MinNavTpl/MinNavTpl.VM/VMs/Page04VM.cs
+++ Src/MinNavTpl/MinNavTpl.VM/VMs/Page04VM.cs
@@ -1,6 +1,8 @@
 ﻿namespace MinNavTpl.VM.VMs;
 public partial class Page04VM : BaseDbVM
 {
+  int _thisCampaign;
+
   public Page04VM(MainVM mvm, ILogger lgr, IConfigurationRoot cfg, IBpr bpr, ISecForcer sec, QStatsRlsContext dbx, IAddChild win, UserSettings stg, SrvrNameStore svr, DtBsNameStore dbs, LetDbChgStore awd) : base(mvm, lgr, cfg, bpr, sec, dbx, win, svr, dbs, awd, stg, 8110) { }
   public override async Task<bool> InitAsync()
   {
@@ -9,9 +11,11 @@ public partial class Page04VM : BaseDbVM
       IsBusy = true;
       var sw = Stopwatch.StartNew();
 
-      await Dbx.Leads.LoadAsync();
+      _thisCampaign = Dbx.Campaigns.Max(r => r.Id);
+
+      await Dbx.Leads.Where(r => r.CampaignId == _thisCampaign).LoadAsync();
       LeadCvs = CollectionViewSource.GetDefaultView(Dbx.Leads.Local.ToObservableCollection());
-      LeadCvs.SortDescriptions.Add(new SortDescription(nameof(Lead.AddedAt), ListSortDirection.Descending));
+      LeadCvs.SortDescriptions.Add(new SortDescription(nameof(Lead.AddedAt), ListSortDirection.Ascending));
       LeadCvs.Filter = obj => obj is not Lead lead || lead is null || string.IsNullOrEmpty(SearchText) ||
         lead.Note?.Contains(SearchText, StringComparison.OrdinalIgnoreCase) == true ||
         lead.OppCompany?.Contains(SearchText, StringComparison.OrdinalIgnoreCase) == true;
@@ -31,6 +35,17 @@ public partial class Page04VM : BaseDbVM
   string _f = ""; public string SearchText { get => _f; set { if (SetProperty(ref _f, value)) LeadCvs?.Refresh(); } }
   bool? _ic; public bool? IncludeClosed { get => _ic; set { if (SetProperty(ref _ic, value)) LeadCvs?.Refresh(); } }
 
-  [RelayCommand] void AddNewLead() {; ; }
+  [RelayCommand]
+  void AddNewLead()
+  {
+    try
+    {
+      var nl = new Lead { AddedAt = DateTime.Now, CampaignId = _thisCampaign, Note = string.IsNullOrEmpty(Clipboard.GetText()) ? "New Lead" : Clipboard.GetText() };
+      Dbx.Leads.Local.Add(nl);
+
+      SelectdLead = nl;
+    }
+    catch (Exception ex) { ex.Pop(); }
+  }
   [RelayCommand] void ChkDb4Cngs() {; ; }
 }
