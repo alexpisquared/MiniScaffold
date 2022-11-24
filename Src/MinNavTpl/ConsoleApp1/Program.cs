e42407e81ace5a7da5cec5c69287bf27d6a40e08
AlexPi <alex.pigida@outlook.com>
2022-11-24 13:52:46 -0500

    wqwqw

--- Src/MinNavTpl/ConsoleApp1/Program.cs
+++ Src/MinNavTpl/ConsoleApp1/Program.cs
@@ -6,15 +6,45 @@ using DB.QStats.Std.Models;
 using Emailing.NET6;
 using GigaHunt.AsLink;
 using Microsoft.EntityFrameworkCore;
+using StandardLib.Consts;
+
 var _now = DateTime.Today;
 var _sw = Stopwatch.StartNew();
 int _cur = 0, _j = 0, _ttl, _page = 1;
 HashSet<string> _vlds = new(), _bads = new();
 
-await ShowPhoneExtraction();
+await ShowBadPhonesFromDB();
+//await ShowPhoneExtraction();
 Console.Write("\a");
 Console.ResetColor();
 
+async Task ShowBadPhonesFromDB()
+{
+  using var dbx = QstatsRlsContext.Create();
+
+  Console.ForegroundColor = ConsoleColor.Cyan;
+
+  var badPhNums = new List<Phone>();
+  await dbx.Phones.OrderBy(r => r.PhoneNumber).ForEachAsync(p =>
+  {
+    if (p.PhoneNumber.Contains("_") || !AreaCodeValidator.Any(p.PhoneNumber))
+      badPhNums.Add(p);
+  });
+
+  _ttl = badPhNums.Count;
+  _sw = Stopwatch.StartNew();
+
+  Console.ForegroundColor = ConsoleColor.Cyan;
+  Console.Write($"   {_ttl} / {dbx.Phones.Count()}   \n");
+  Console.ForegroundColor = ConsoleColor.DarkCyan;
+  badPhNums.ToList().ForEach(p => Console.Write($"   {++_cur,5}    {p.PhoneNumber}   {p.SeenLast}   {(p.SeenLast - p.SeenFirst).TotalDays,5:N1}  \n"));
+
+  var rv = await RemoveBadsFromDbAsync(dbx, badPhNums.Select(r => r.PhoneNumber));
+
+  var rowsSaved = await dbx.SaveChangesAsync();
+  Console.ForegroundColor = rowsSaved > 0 ? ConsoleColor.Green : ConsoleColor.DarkGray;
+  Console.WriteLine($"       {(rowsSaved > 0 ? "■ ■ ■ ■ ==>" : "··········")}  {rowsSaved,5} rowsSaved saved.     All took {_sw.Elapsed.TotalSeconds,5:N0} sec.");
+}
 async Task ShowPhoneExtraction()
 {
   using var dbx = QstatsRlsContext.Create();
