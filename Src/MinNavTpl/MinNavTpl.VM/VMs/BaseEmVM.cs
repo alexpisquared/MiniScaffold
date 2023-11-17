8007dfb20462f4cbaa48c5de2db8fe45a2dcab18
alexpisqueared <alex.pigida@outlook.com>
2023-11-17 14:27:58 -0500

    net8 upgrade

--- Src/MinNavTpl/MinNavTpl.VM/VMs/BaseEmVM.cs
+++ Src/MinNavTpl/MinNavTpl.VM/VMs/BaseEmVM.cs
@@ -2,31 +2,50 @@
 
 public partial class BaseEmVM : BaseDbVM
 {
-  protected List<string>? _badEmails;
-  int _thisCampaign;
-  public BaseEmVM(MainVM mvm, ILogger lgr, IConfigurationRoot cfg, IBpr bpr, ISecurityForcer sec, QstatsRlsContext dbx, IAddChild win, SrvrNameStore svr, DtBsNameStore dbs, GSReportStore gsr, LetDbChgStore awd, UserSettings stg, EmailOfIStore eml, EmailDetailVM evm, int oid) : base(mvm, lgr, cfg, bpr, sec, dbx, win, svr, dbs, gsr, awd, stg, oid)
-  {
-    EmailOfIStore = eml; //EmailOfIStore.Changed += EmailOfIStore_Chngd;
-    EmailOfIVM = evm;
-  }
+    protected List<string>? _badEmails;
+    int _thisCampaign;
+    public BaseEmVM(MainVM mvm, ILogger lgr, IConfigurationRoot cfg, IBpr bpr, ISecurityForcer sec, QstatsRlsContext dbx, IAddChild win, SrvrNameStore svr, DtBsNameStore dbs, GSReportStore gsr, LetDbChgStore awd, UserSettings stg, EmailOfIStore eml, EmailDetailVM evm, int oid) : base(mvm, lgr, cfg, bpr, sec, dbx, win, svr, dbs, gsr, awd, stg, oid)
+    {
+        EmailOfIStore = eml; //EmailOfIStore.Changed += EmailOfIStore_Chngd;
+        EmailOfIVM = evm;
+    }
 
-  public async override Task<bool> InitAsync()
-  {
-    await Task.Delay(22); // <== does not show up without this...............................
+    public async override Task<bool> InitAsync()
+    {
+        await Task.Delay(22); // <== does not show up without this...............................
+        try
+        {
+            _thisCampaign = Dbx.Campaigns.Max(r => r.Id);
 
-    _thisCampaign = Dbx.Campaigns.Max(r => r.Id);
+            _badEmails = await MiscEfDb.GetBadEmails("Select Id from [dbo].[BadEmails]()", Dbx.Database.GetConnectionString() ?? "??");
+        }
+        catch (Exception ex) { ex.Pop(Lgr); return false; }
 
-    _badEmails = await MiscEfDb.GetBadEmails("Select Id from [dbo].[BadEmails]()", Dbx.Database.GetConnectionString() ?? "??");
+        return await base.InitAsync();
+    }
 
-    return await base.InitAsync();
-  }
+    public EmailOfIStore EmailOfIStore
+    {
+        get;
+    }
+    public EmailDetailVM EmailOfIVM
+    {
+        get;
+    }
 
-  public EmailOfIStore EmailOfIStore { get; }
-  public EmailDetailVM EmailOfIVM { get; }
-
-
-
-  [RelayCommand] protected void Nxt() { Bpr.Click(); try { WriteLine(PageCvs?.MoveCurrentToNext()); } catch (Exception ex) { ex.Pop(); } }
-  [RelayCommand] void OLk() { Bpr.Click(); try { _ = MessageBox.Show("■"); } catch (Exception ex) { ex.Pop(); } }
-  [RelayCommand] void DNN() { Bpr.Click(); try { _ = MessageBox.Show("■"); } catch (Exception ex) { ex.Pop(); } }
+    [RelayCommand]
+    protected void Nxt()
+    {
+        Bpr.Click(); try { WriteLine(PageCvs?.MoveCurrentToNext()); } catch (Exception ex) { ex.Pop(); }
+    }
+    [RelayCommand]
+    void OLk()
+    {
+        Bpr.Click(); try { _ = MessageBox.Show("■"); } catch (Exception ex) { ex.Pop(); }
+    }
+    [RelayCommand]
+    void DNN()
+    {
+        Bpr.Click(); try { _ = MessageBox.Show("■"); } catch (Exception ex) { ex.Pop(); }
+    }
 }
