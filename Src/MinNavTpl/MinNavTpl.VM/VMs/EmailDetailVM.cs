3659b6f60352fdd1fb8d64bfeab5145f36313f6d
alexpisqueared <alex.pigida@outlook.com>
2024-01-03 14:40:38 -0500

    454

--- Src/MinNavTpl/MinNavTpl.VM/VMs/EmailDetailVM.cs
+++ Src/MinNavTpl/MinNavTpl.VM/VMs/EmailDetailVM.cs
@@ -1,21 +1,26 @@
-﻿namespace MinNavTpl.VM.VMs;
+﻿using StandardLib.Extensions;
+
+namespace MinNavTpl.VM.VMs;
 public partial class EmailDetailVM : BaseDbVM
 {
-  public EmailDetailVM(MainVM mvm, ILogger lgr, IConfigurationRoot cfg, IBpr bpr, ISecurityForcer sec, QstatsRlsContext dbq, IAddChild win, UserSettings stg, SrvrNameStore svr, DtBsNameStore dbs, GSReportStore gsr, EmailOfIStore eml, LetDbChgStore awd, ISpeechSynth synth) : base(mvm, lgr, cfg, bpr, sec, dbq, win, svr, dbs, gsr, awd, stg, synth, 8110)
-  {
-    EmailOfIStore = eml; EmailOfIStore.Changed += EmailOfIStore_Chngd;
-    EmailOfI = eml.LastVal;
-  }
-  public async override Task<bool> InitAsync() { IsBusy = true; _ = await InitAsyncTask(EmailOfI); return await base.InitAsync(); }
-  async Task<bool> InitAsyncTask(string emailOfI, [CallerMemberName] string? cmn = "")
-  {
-    WriteLine($"■■ Init  {GetCaller(),20}  called by  {cmn,-22} {emailOfI,-22} ■■■■");
-    try
+    public EmailDetailVM(MainVM mvm, ILogger lgr, IConfigurationRoot cfg, IBpr bpr, ISecurityForcer sec, QstatsRlsContext dbq, IAddChild win, UserSettings stg, SrvrNameStore svr, DtBsNameStore dbs, GSReportStore gsr, EmailOfIStore eml, LetDbChgStore awd, ISpeechSynth synth) : base(mvm, lgr, cfg, bpr, sec, dbq, win, svr, dbs, gsr, awd, stg, synth, 8110)
     {
-      var sw = Stopwatch.StartNew();
-      EmailOfI = emailOfI;
+        EmailOfIStore = eml; EmailOfIStore.Changed += EmailOfIStore_Chngd;
+        EmailOfI = eml.LastVal;
+    }
+    public async override Task<bool> InitAsync()
+    {
+        IsBusy = true; _ = await InitAsyncTask(EmailOfI); return await base.InitAsync();
+    }
+    async Task<bool> InitAsyncTask(string emailOfI, [CallerMemberName] string? cmn = "")
+    {
+        WriteLine($"■■ Init  {GetCaller(),20}  called by  {cmn,-22} {emailOfI,-22} ■■■■");
+        try
+        {
+            var sw = Stopwatch.StartNew();
+            EmailOfI = emailOfI;
 
-      await Task.Yield();
+            await Task.Yield();
 
 #if !true
       await Dbq.
@@ -24,41 +29,50 @@ public partial class EmailDetailVM : BaseDbVM
         LoadAsync();
 #else //^^ VS vv    //todo: https://learn.microsoft.com/en-us/aspnet/core/data/ef-mvc/read-related-data?view=aspnetcore-6.0
 
-      //This type of CollectionView does not support changes to its SourceCollection from a thread different from the Dispatcher thread.   NotSupportedException at C:\g\MiniScaffold\Src\MinNavTpl\MinNavTpl.VM\VMs\EmailDetailVM.cs(40): InitAsyncTask() 
-      //System.NotSupportedException: This type of CollectionView does not support changes to its SourceCollection from a thread different from the Dispatcher thread.
-      //await Dbq.Emails.Where(r => r.Id == EmailOfI).LoadAsync();
-      //await Dbq.Ehists.Where(r => r.EmailId == EmailOfI).LoadAsync();
-      Dbq.Emails.Where(r => r.Id == EmailOfI).Load();
-      Dbq.Ehists.Where(r => r.EmailId == EmailOfI).Load();
+            //This type of CollectionView does not support changes to its SourceCollection from a thread different from the Dispatcher thread.   NotSupportedException at C:\g\MiniScaffold\Src\MinNavTpl\MinNavTpl.VM\VMs\EmailDetailVM.cs(40): InitAsyncTask() 
+            //System.NotSupportedException: This type of CollectionView does not support changes to its SourceCollection from a thread different from the Dispatcher thread.
+            //await Dbq.Emails.Where(r => r.Id == EmailOfI).LoadAsync();
+            //await Dbq.Ehists.Where(r => r.EmailId == EmailOfI).LoadAsync();
+            Dbq.Emails.Where(r => r.Id == EmailOfI).Load();
+            if (Dbq.Ehists.Any(r => r.EmailId == EmailOfI))
+                try { Dbq.Ehists.Where(r => r.EmailId == EmailOfI).Load(); }
+                catch (Exception ex)
+                {
+                    var sql = Dbq.Ehists.Where(r => r.EmailId == EmailOfI).ToQueryString(); // SELECT[e].[ID], [e].[AddedAt], [e].[EMailID], [e].[EmailedAt], [e].[LetterBody], [e].[LetterSubject], [e].[Notes], [e].[RecivedOrSent], [e].[SentOn] FROM[EHist] AS[e] WHERE[e].[EMailID] = 'jill.mellish@robertson.ca'
+                    ex.Log($"{emailOfI} //todo: Why LetterSubject=null causes the issue?\n  {sql}");
+                }
 #endif
 
-      PageCvs = CollectionViewSource.GetDefaultView(Dbq.Ehists.Local.ToObservableCollection().Where(r => r.EmailId == EmailOfI)); //tu: ?? instead of .LoadAsync() / .Local.ToObservableCollection() ?? === PageCvs = CollectionViewSource.GetDefaultView(await Dbq.Ehists.ToListAsync());
-      PageCvs.SortDescriptions.Add(new SortDescription(nameof(Ehist.AddedAt), ListSortDirection.Descending));
-      //PageCvs.Filter = obj => obj is not Ehist lead || lead is null || string.IsNullOrEmpty(SearchText) ||        lead.Id.Contains(SearchText, StringComparison.OrdinalIgnoreCase) == true ||        lead.Notes?.Contains(SearchText, StringComparison.OrdinalIgnoreCase) == true;
+            PageCvs = CollectionViewSource.GetDefaultView(Dbq.Ehists.Local.ToObservableCollection().Where(r => r.EmailId == EmailOfI)); //tu: ?? instead of .LoadAsync() / .Local.ToObservableCollection() ?? === PageCvs = CollectionViewSource.GetDefaultView(await Dbq.Ehists.ToListAsync());
+            PageCvs.SortDescriptions.Add(new SortDescription(nameof(Ehist.AddedAt), ListSortDirection.Descending));
+            //PageCvs.Filter = obj => obj is not Ehist lead || lead is null || string.IsNullOrEmpty(SearchText) ||        lead.Id.Contains(SearchText, StringComparison.OrdinalIgnoreCase) == true ||        lead.Notes?.Contains(SearchText, StringComparison.OrdinalIgnoreCase) == true;
 
-      GSReport = $" {Dbq.Emails.Local.Count:N0} + {Dbq.Ehists.Local.Count:N0} / {sw.Elapsed.TotalSeconds:N1} loaded rows / s";
-      //Lgr.Log(LogLevel.Trace, GSReport );
+            GSReport = $" {Dbq.Emails.Local.Count:N0} + {Dbq.Ehists.Local.Count:N0} / {sw.Elapsed.TotalSeconds:N1} loaded rows / s";
+            //Lgr.Log(LogLevel.Trace, GSReport );
 
-      return true;
+            return true;
+        }
+        catch (Exception ex) { GSReport = $"FAILED. \r\n  {ex.Message}"; ex.Pop(Lgr); return false; }
+        finally { IsBusy = false; }
     }
-    catch (Exception ex) { GSReport = $"FAILED. \r\n  {ex.Message}"; ex.Pop(Lgr); return false; }
-    finally { IsBusy = false; }
-  }
-  public override void Dispose()
-  {
-    EmailOfIStore.Changed -= EmailOfIStore_Chngd;
-    base.Dispose();
-  }
+    public override void Dispose()
+    {
+        EmailOfIStore.Changed -= EmailOfIStore_Chngd;
+        base.Dispose();
+    }
 
-  async void EmailOfIStore_Chngd(string emailOfI, [CallerMemberName] string? cmn = "")
-  {
-    WriteLine($"■■ EmDt  {GetCaller(),20}  called by  {cmn,-22} {emailOfI,-22}  {(EmailOfI != emailOfI ? "==>Load" : "==>----")}");
-    if (EmailOfI != emailOfI)
-      _ = await InitAsyncTask(emailOfI);
-  }
-  public EmailOfIStore EmailOfIStore { get; }
+    async void EmailOfIStore_Chngd(string emailOfI, [CallerMemberName] string? cmn = "")
+    {
+        WriteLine($"■■ EmDt  {GetCaller(),20}  called by  {cmn,-22} {emailOfI,-22}  {(EmailOfI != emailOfI ? "==>Load" : "==>----")}");
+        if (EmailOfI != emailOfI)
+            _ = await InitAsyncTask(emailOfI);
+    }
+    public EmailOfIStore EmailOfIStore
+    {
+        get;
+    }
 
-  [ObservableProperty] string emailOfI = "";
-  [ObservableProperty] Ehist? currentEhist;
-  [ObservableProperty] Ehist? selectdEhist;
+    [ObservableProperty] string emailOfI = "";
+    [ObservableProperty] Ehist? currentEhist;
+    [ObservableProperty] Ehist? selectdEhist;
 }
--- Src/MinNavTpl/MinNavTpl.VM/VMs/Page02VM.cs
+++ Src/MinNavTpl/MinNavTpl.VM/VMs/Page02VM.cs
@@ -5,44 +5,52 @@ public partial class Page02VM : BaseEmVM
       : base(mvm, lgr, cfg, bpr, sec, dbq, win, svr, dbs, gsr, awd, stg, eml, evm, synth, 8110) { }
     public async override Task<bool> InitAsync()
     {
-        try
+        for (int i = 0; i < 26; i++)
         {
-            await Task.Delay(20); // <== does not show up without this...............................
-            await Bpr.StartAsync(8);
-            IsBusy = true;
-
-            var sw = Stopwatch.StartNew();
-
-            DailyDose = // DevOps.IsDbg ? 2 : 
-                UsrStgns.DailyDose;
-
-            await Dbq.PhoneEmailXrefs.LoadAsync();
-            await Dbq.Phones.LoadAsync();
-
-            var emailQuery = // DevOps.IsDbg ? Dbq.Emails.Where(r => IncludeClosed || r.Id.Contains("reply.l") || r.Id.Contains("reply.f") || r.Id.Contains("reply.f")).OrderBy(r => r.LastAction) :
-                Dbq.Emails.Where(r => IncludeClosed || Dbq.VEmailIdAvailProds.Select(r => r.Id).Contains(r.Id) == true).OrderBy(r => r.NotifyPriority);
-            await emailQuery.LoadAsync(); //tmi: Lgr.Log(LogLevel.Trace, emailQuery.ToQueryString());
-
-            PageCvs = CollectionViewSource.GetDefaultView(Dbq.Emails.Local.ToObservableCollection()); //tu: ?? instead of .LoadAsync() / .Local.ToObservableCollection() ?? === PageCvs = CollectionViewSource.GetDefaultView(await Dbq.VEmailAvailProds.ToListAsync());
-            //redundant: PageCvs.SortDescriptions.Add(new SortDescription(nameof(Email.AddedAt), ListSortDirection.Descending));
-            PageCvs.Filter = obj => obj is not Email r || r is null || string.IsNullOrEmpty(SearchText) ||
-              r.Id.Contains(SearchText, StringComparison.OrdinalIgnoreCase) == true ||
-              r.Notes?.Contains(SearchText, StringComparison.OrdinalIgnoreCase) == true;
-
-            _ = PageCvs?.MoveCurrentToFirst();
-            await GetTopDetailAsync();
-
-            GSReport = $"╞══ Emails: {PageCvs?.Cast<Email>().Count():N0} / {sw.Elapsed.TotalSeconds:N1} sec ";
-            Lgr.Log(LogLevel.Trace, GSReport);
-
-            if (Environment.GetCommandLineArgs().Contains("Broad") && (DateTimeOffset.Now - DevOps.AppStartedAt).TotalSeconds < antiSpamSec)
+            try
             {
-                await SendTopNAsync();
-            }
+                await Task.Delay(20); // <== does not show up without this...............................
+                await Bpr.StartAsync(8);
+                IsBusy = true;
 
-            await Bpr.FinishAsync(8);
-            return await base.InitAsync();
-        } catch (Exception ex) { GSReport = $"FAILED. \r\n  {ex.Message}"; ex.Pop(Lgr); return false; } finally { IsBusy = false; }
+                var sw = Stopwatch.StartNew();
+
+                DailyDose = // DevOps.IsDbg ? 2 : 
+                    UsrStgns.DailyDose;
+
+                await Dbq.PhoneEmailXrefs.LoadAsync();
+                await Dbq.Phones.LoadAsync();
+
+                var emailQuery = // DevOps.IsDbg ? Dbq.Emails.Where(r => IncludeClosed || r.Id.Contains("reply.l") || r.Id.Contains("reply.f") || r.Id.Contains("reply.f")).OrderBy(r => r.LastAction) :
+                    Dbq.Emails.Where(r => IncludeClosed || Dbq.VEmailIdAvailProds.Select(r => r.Id).Contains(r.Id) == true).OrderBy(r => r.NotifyPriority);
+                await emailQuery.LoadAsync(); //tmi: Lgr.Log(LogLevel.Trace, emailQuery.ToQueryString());
+
+                PageCvs = CollectionViewSource.GetDefaultView(Dbq.Emails.Local.ToObservableCollection()); //tu: ?? instead of .LoadAsync() / .Local.ToObservableCollection() ?? === PageCvs = CollectionViewSource.GetDefaultView(await Dbq.VEmailAvailProds.ToListAsync());
+                                                                                                          //redundant: PageCvs.SortDescriptions.Add(new SortDescription(nameof(Email.AddedAt), ListSortDirection.Descending));
+                PageCvs.Filter = obj => obj is not Email r || r is null || string.IsNullOrEmpty(SearchText) ||
+                  r.Id.Contains(SearchText, StringComparison.OrdinalIgnoreCase) == true ||
+                  r.Notes?.Contains(SearchText, StringComparison.OrdinalIgnoreCase) == true;
+
+                _ = PageCvs?.MoveCurrentToFirst();
+                await GetTopDetailAsync();
+
+                GSReport = $"╞══ Emails: {PageCvs?.Cast<Email>().Count():N0} / {sw.Elapsed.TotalSeconds:N1} sec ";
+                Lgr.Log(LogLevel.Trace, GSReport);
+
+                if (Environment.GetCommandLineArgs().Contains("Broad") && (DateTimeOffset.Now - DevOps.AppStartedAt).TotalSeconds < antiSpamSec)
+                {
+                    await SendTopNAsync();
+                }
+
+                await Bpr.FinishAsync(8);
+                return await base.InitAsync();
+            }
+            catch (SqlException ex) { GSReport = $"FAILED {i,2}/{26}  {ex.Message}"; Synth.SpeakFAF("Retrying many times."); }
+            catch (Exception ex) { GSReport = $"FAILED. \r\n  {ex.Message}"; ex.Pop(Lgr); return false; }
+            finally { IsBusy = false; }
+        } // for
+
+        return false;
     }
 
     [ObservableProperty] int dailyDose; partial void OnDailyDoseChanged(int value)
