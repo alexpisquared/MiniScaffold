e04e0f99a1f04c5d8bb82683f51f4f68ecf1b823
alexpisqueared <alex.pigida@outlook.com>
2024-02-06 17:25:41 -0500

    pau

--- Src/MinNavTpl/MinNavTpl.VM/VMs/BaseEmVM.cs
+++ Src/MinNavTpl/MinNavTpl.VM/VMs/BaseEmVM.cs
@@ -74,8 +74,8 @@ public partial class BaseEmVM : BaseDbVM
 
             GSReport += $"{fName,-15}\t{email,-47}";
 
-            var timestamp = DateTime.Now;
-            var (success, report1) = await QStatusBroadcaster.SendLetter(email, fName, isAvailable: true, timestamp, Lgr);
+            var now = DateTime.Now;
+            var (success, report1) = await QStatusBroadcaster.SendLetter(email, fName, isAvailable: true, now, Lgr);
             if (success)
             {
                 var em = Dbq.Emails.FirstOrDefault(r => r.Id == email && r.ReSendAfter != null);
@@ -84,8 +84,10 @@ public partial class BaseEmVM : BaseDbVM
                     em.ReSendAfter = null;
                 }
 
+                Lgr.Log(LogLevel.Information, $":sent/timestamped: {now:HH:mm:ss.f} {fName,26}\t{email,-47}");
+
                 GSReport += "\r\n";
-                _ = await new OutlookToDbWindowHelpers(Lgr).CheckInsert_EMail_EHist_Async(Dbq, email, fName, "", "asu .net 8.0 - success", "ASU - 4 CVs - 2023-12", timestamp, timestamp, "..from std broadcast send", "S");
+                _ = await new OutlookToDbWindowHelpers(Lgr).CheckInsert_EMail_EHist_Async(Dbq, email, fName, "", "asu .net 8.0 - success", "ASU - 4 CVs - 2023-12", now, now, "..from std broadcast send", "S");
             }
             else
             {
